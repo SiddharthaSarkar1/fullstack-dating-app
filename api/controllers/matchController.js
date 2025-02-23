58fe866f6cb1d384becc61d00124822ee4e0ed41
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-02-23 21:39:17 +0530

    add messageController

--- api/controllers/matchController.js
+++ api/controllers/matchController.js
@@ -1,6 +1,45 @@
 import User from "../models/User.model.js";
 
-export const swipeRight = async (req, res) => {};
+export const swipeRight = async (req, res) => {
+  try {
+    const { likedUserId } = req.params;
+    const currentUser = await User.findById(req.user.id);
+    const likedUser = await User.findById(likedUserId);
+
+    if (!likedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!currentUser.likes.includes(likedUserId)) {
+      currentUser.likes.push(likedUserId);
+      await currentUser.save();
+
+      //If the other ser already like the current user, it is already a match.So lets update the both users
+      if (likedUser.likes.includes(currentUser.id)) {
+        currentUser.matches.push(likedUserId);
+        likedUser.matches.push(currentUser.id);
+        await Promise.all([await currentUser.save(), await likedUser.save()]);
+      }
+
+      //TODO: Send notification if it is a match
+    }
+
+    res.status(200).json({
+      success: true,
+      user: currentUser,
+    });
+  } catch (error) {
+    console.log("Error in swipeRight: ", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
 export const swipeLeft = async (req, res) => {
   try {
