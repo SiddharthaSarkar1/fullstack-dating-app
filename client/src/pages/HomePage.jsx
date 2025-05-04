a0ffb904e1525a58a1c809028b27d82617a8847d
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-05-04 23:41:11 +0530

    sidebar UI development

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
+import Sidebar from "../components/Sidebar";
 import { useAuthStore } from "../store/useAuthStore";
 
 const HomePage = () => {
   const { logout } = useAuthStore();
   return (
-    <div>
-      Homepage
-      <button onClick={logout}>Logout</button>
+    <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to-purple-100 overflow-hidden">
+      <Sidebar />
     </div>
   );
 };
