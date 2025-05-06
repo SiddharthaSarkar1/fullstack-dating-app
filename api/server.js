60fa58505cd11fb68ef517c416fa708a975bbbce
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-05-06 22:28:57 +0530

    Add socket.io setup for server

--- api/server.js
+++ api/server.js
@@ -3,23 +3,29 @@ import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { createServer } from "http";
 
 //Routes
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import matchRoutes from "./routes/matchRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
+import { initializeSocket } from "./socket/socket.server.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
@@ -28,7 +34,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
   connectDB(DATABASE_URL);
 });
